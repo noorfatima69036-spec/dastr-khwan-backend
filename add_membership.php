57d7a5f7264458e536610558769b408c8842236f
noorfatima69036-spec <noorfatima69036-spec@gmail.com>
2026-09-23 09:16:41 +0500

    Updated project changes

--- add_membership.php
+++ add_membership.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit();
 }
 // add_membership.php
-require_once '../config/db_connect.php';
+require_once 'config/db_connect.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
