308bae75bf207339cf49d09ea9f1dc65b57d3920
noorfatima69036-spec <noorfatima69036-spec@gmail.com>
2026-09-23 06:28:08 +0500

    Updated project changes

--- admin/add_membership.php
+++ admin/add_membership.php
@@ -1,4 +1,13 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 // add_membership.php
 require_once '../config/db_connect.php';
 
